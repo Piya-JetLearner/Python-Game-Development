79cf86e487aac5b950940a596753d1a99d896dcc
Piya-JetLearner <piyajhp@gmail.com>
2025-12-12 19:26:39 +0000

    Add files via upload

--- clicking game.py
+++ clicking game.py
@@ -7,20 +7,27 @@ hp=Actor("harry potter clipart")
 hp.x=300
 hp.y=250
 hg=Actor("hermione granger clipart")
+game=True
 def draw():
     screen.fill("pink")
-    hp.draw()
-    hg.draw()
-    screen.draw.text("click me",(100,100))
-    screen.draw.text(f"score={score}",(450,30))
-    
+    if game==True:
+        hp.draw()
+        hg.draw()
+        screen.draw.text("click me",(100,100))
+        screen.draw.text(f"score={score}",(450,30))
+    else:
+        screen.fill("red")
+        screen.draw.text("GAME OVER",(300,250))
 def on_mouse_down(pos):
-    global score
+    global score,game
     print(pos)
     if hp.collidepoint(pos):
         hp.x= random.randint(50,500)
         hp.y= random.randint(50,250)
         score=score+1
+    if hg.collidepoint(pos):
+        game=False
+        
 def spawn_hg():
     hg.x=random.randint(50,500)
     hg.y=random.randint(50,250)
